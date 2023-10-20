ee0441ec646282fe6a54ad20f7cd30a37ce409b1
Intern3pk <Intern3pk@DESKTOP-QPK-01>
2023-10-20 18:27:48 +0500

    latest code

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import productData from './Components/ProductData';
+
+function App() {
+  const categories = [...new Set(productData.map(item => item.category))];
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [showSubcategories, setShowSubcategories] = useState(false);
+
+  const subcategories = productData
+    .filter(product => product.category === selectedCategory)
+    .map(product => product.productName);
+
+  const handleMouseEnter = (category) => {
+    setSelectedCategory(category);
+    setShowSubcategories(true);
+  };
+
+  const handleMouseLeave = () => {
+    setSelectedCategory(null);
+    setShowSubcategories(false);
+  };
+
+  const filteredProducts = productData.filter(product => {
+    return (
+      (!selectedCategory || product.category === selectedCategory) &&
+      (!selectedProduct || product.productName === selectedProduct)
+    );
+  });
+
+  return (
+    <div className="App">
+      <header>
+        <nav className="flex justify-between items-center p-4 bg-gray-500 text-white h-9">
+          <div className="logo">Your Logo</div>
+          <ul className="categories flex space-x-4">
+            {categories.map(category => (
+              <li
+                key={category}
+                onMouseEnter={() => handleMouseEnter(category)}
+                onMouseLeave={handleMouseLeave}
+                className={`cursor-pointer ${selectedCategory === category ? 'bg-blue-500' : ''}`}
+              >
+                {category}
+                {showSubcategories && selectedCategory === category && (
+                  <ul className="subcategories space-y-2">
+                    {subcategories.map(subcategory => (
+                      <li
+                        key={subcategory}
+                        onClick={() => setSelectedProduct(subcategory)}
+                        className="cursor-pointer"
+                      >
+                        {subcategory}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div className="product-list">
+          {filteredProducts.map(product => (
+            <div key={product.productName} className="product-card border p-4">
+              <img src={product.imageUrl} alt={product.productName} className="max-w-xs mx-auto" />
+              <h3
+                onClick={() => setSelectedProduct(product.productName)}
+                className="cursor-pointer text-xl font-bold mt-2"
+              >
+                {product.productName}
+              </h3>
+              <p>Brand: {product.brand}</p>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ProductData.jsx
@@ -0,0 +1,855 @@
+const productData=[
+    {
+        brand: "Tapal Tea",
+        productName: "Tea ",
+        category: "Food"
+    },
+    {
+        brand: "Vital Tea",
+        productName: "Tea ",
+        category: "Food"
+    },
+    {
+        brand: "Qamar Tea",
+        productName: "Tea ",
+        category: "Food"
+    },
+    {
+        brand: "Meezan Tea",
+        productName: "Tea ",
+        category: "Food"
+    },
+    {
+        brand: "Capri",
+        productName: "Hand Wash",
+        category: "grocery"
+    },
+    {
+        brand: "Necos",
+        productName: "Hand Wash",
+        category: "grocery"
+    },
+    {
+        brand: "Bio Amla",
+        productName: "Hair Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Hamdard",
+        productName: "Hair Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Hemani",
+        productName: "Hair Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Saeed Ghani",
+        productName: "Hair Oil",
+        category: "grocery"
+    },
+    {
+        brand: "United",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Power",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Super Power",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Ravi",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Road Prince",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Unique",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Crown",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Eagle",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Ghani",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Hero",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Hi-Speed",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Metro",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Pak Hero",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Sohrab",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Super Asia",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Star",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Supreme",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Treet Motorcycle",
+        productName: "Bikes",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Orient",
+        productName: "TVs",
+        category: "Electronics"
+    },
+    {
+        brand: "PEL",
+        productName: "TVs",
+        category: "Electronics"
+    },
+    {
+        brand: "Orange",
+        productName: "TVs",
+        category: "Electronics"
+    },
+    {
+        brand: "PEL Refrigerators",
+        productName: "Refrigerators",
+        category: "Electronics"
+    },
+    {
+        brand: "Orient Refrigerators",
+        productName: "Refrigerators",
+        category: "Electronics"
+    },
+    {
+        brand: "Waves Singer Pakistan",
+        productName: "Refrigerators",
+        category: "Electronics"
+    },
+    {
+        brand: "Capri",
+        productName: "Soaps",
+        category: "grocery"
+    },
+    {
+        brand: "Tibet",
+        productName: "Soaps",
+        category: "grocery"
+    },
+    {
+        brand: "Vital",
+        productName: "Soaps",
+        category: "grocery"
+    },
+    {
+        brand: "Sufi",
+        productName: "Soaps",
+        category: "grocery"
+    },
+    {
+        brand: "Hoor",
+        productName: "Soaps",
+        category: "grocery"
+    },
+    {
+        brand: "Care",
+        productName: "Soaps",
+        category: "grocery"
+    },
+    {
+        brand: "Trust",
+        productName: "Sanitary Pads",
+        category: "grocery"
+    },
+    {
+        brand: "Butterfly",
+        productName: "Sanitary Pads",
+        category: "grocery"
+    },
+    {
+        brand: "Spot-free",
+        productName: "Sanitary Pads",
+        category: "grocery"
+    },
+    {
+        brand: "Leo",
+        productName: "Baby Diapers",
+        category: "grocery"
+    },
+    {
+        brand: "Bonapapa",
+        productName: "Baby Diapers",
+        category: "grocery"
+    },
+    {
+        brand: "Mom & Me",
+        productName: "Baby Products",
+        category: "grocery"
+    },
+    {
+        brand: "Treet Razor",
+        productName: "Shaving Razors",
+        category: "grocery"
+    },
+    {
+        brand: "Khaadi",
+        productName: "Clothes",
+        category: "Fasion"
+    },
+    {
+        brand: "Warda",
+        productName: "Clothes",
+        category: "Fasion"
+    },
+    {
+        brand: "Nishat",
+        productName: "Clothes",
+        category: "Fasion"
+    },
+    {
+        brand: "Gul Amhed",
+        productName: "Clothes",
+        category: "Fasion"
+    },
+    {
+        brand: "Kolson",
+        productName: "Pasta",
+        category: "Food"
+    },
+    {
+        brand: "Bake Parlor",
+        productName: "Pasta",
+        category: "Food"
+    },
+    {
+        brand: "Gourmet",
+        productName: "Cold Drinks",
+        category: "Food"
+    },
+    {
+        brand: "Pakola",
+        productName: "Cold Drinks",
+        category: "Food"
+    },
+    {
+        brand: "Cola Next",
+        productName: "Cold Drinks",
+        category: "Food"
+    },
+    {
+        brand: "Medicam",
+        productName: "Toothpastes",
+        category: "grocery"
+    },
+    {
+        brand: "English",
+        productName: "Toothpastes",
+        category: "grocery"
+    },
+    {
+        brand: "Forhan's",
+        productName: "Toothpastes",
+        category: "grocery"
+    },
+    {
+        brand: "Revand",
+        productName: "Toothpastes",
+        category: "grocery"
+    },
+    {
+        brand: "Dentonic",
+        productName: "Toothpastes",
+        category: "grocery"
+    },
+    {
+        brand: "Prema",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Haleeb",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Good Milk",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Adams",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Nurpur",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Millac",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Dayfresh",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Anhar",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Olper's",
+        productName: "Milk ",
+        category: "Food"
+    },
+    {
+        brand: "Dalda Oils",
+        productName: "Cooking Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Seasons Oil",
+        productName: "Cooking Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Sufi",
+        productName: "Cooking Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Sultan",
+        productName: "Cooking Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Olivola",
+        productName: "Cooking Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Meezan ",
+        productName: "Cooking Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Kisan",
+        productName: "Cooking Oil",
+        category: "grocery"
+    },
+    {
+        brand: "Novella",
+        productName: "Chocolates",
+        category: "Food"
+    },
+    {
+        brand: "Crown",
+        productName: "Chocolates",
+        category: "Food"
+    },
+    {
+        brand: "Now",
+        productName: "Chocolates",
+        category: "Food"
+    },
+    {
+        brand: "Sonnet",
+        productName: "Chocolates",
+        category: "Food"
+    },
+    {
+        brand: "Mello",
+        productName: "Chocolates",
+        category: "Food"
+    },
+    {
+        brand: "Silk",
+        productName: "Chocolates",
+        category: "Food"
+    },
+    {
+        brand: "Opus",
+        productName: "Chocolates",
+        category: "Food"
+    },
+    {
+        brand: "Young's",
+        productName: "Chocolate Spreads",
+        category: "Food"
+    },
+    {
+        brand: "Oye Hoye",
+        productName: "Chocolate Spreads",
+        category: "Food"
+    },
+    {
+        brand: "Knock Out",
+        productName: "Chocolate Spreads",
+        category: "Food"
+    },
+    {
+        brand: "Qmobile",
+        productName: "Mobile Phone",
+        category: "Electronics"
+    },
+    {
+        brand: "Rivo Mobile",
+        productName: "Mobile Phone",
+        category: "Electronics"
+    },
+    {
+        brand: "Club Mobile",
+        productName: "Mobile Phone",
+        category: "Electronics"
+    },
+    {
+        brand: "United Bravo",
+        productName: "Cars",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Prince Pearl REX7",
+        productName: "Cars",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Suzuki Mehran",
+        productName: "Cars",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Suzuki Alto",
+        productName: "Cars",
+        category: "Auto-Mobiles"
+    },
+    {
+        brand: "Treet Corporation",
+        productName: "Shaving Products",
+        category: "grocery"
+    },
+    {
+        brand: "Tibet Shaving Cream",
+        productName: "Shaving Products",
+        category: "grocery"
+    },
+    {
+        brand: "Samsol Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Tibet Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Olivia Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Nisa Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Parley Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Medora Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Forvil Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Care Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Golden Pearl Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Bio Amla",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "English",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Poonia Brothers",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Saeed Ghani Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Skin White",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Forhan's Cosmetics",
+        productName: "Cosmetics Products",
+        category: "grocery"
+    },
+    {
+        brand: "Medora",
+        productName: "Makeup Products",
+        category: "grocery"
+    },
+    {
+        brand: "Swiss Miss",
+        productName: "Makeup Products",
+        category: "grocery"
+    },
+    {
+        brand: "Nabeela",
+        productName: "Makeup Products",
+        category: "grocery"
+    },
+    {
+        brand: "Mussarat Misbah",
+        productName: "Makeup Products",
+        category: "grocery"
+    },
+    {
+        brand: "Sweet Touch",
+        productName: "Makeup Products",
+        category: "grocery"
+    },
+    {
+        brand: "Luscious",
+        productName: "Makeup Products",
+        category: "grocery"
+    },
+    {
+        brand: "Odho",
+        productName: "Makeup Products",
+        category: "grocery"
+    },
+    {
+        brand: "J.Makeup",
+        productName: "Makeup Products",
+        category: "grocery"
+    },
+    {
+        brand: "Shezan",
+        productName: "Juices & Squashes",
+        category: "Food"
+    },
+    {
+        brand: "Mitchell's",
+        productName: "Juices & Squashes",
+        category: "Food"
+    },
+    {
+        brand: "Refresh,Anytime by Shakarganj",
+        productName: "Juices & Squashes",
+        category: "Food"
+    },
+    {
+        brand: "Fruiti-O by Shangrilla Pvt Limited",
+        productName: "Juices & Squashes",
+        category: "Food"
+    },
+    {
+        brand: "Fruitien",
+        productName: "Juices & Squashes",
+        category: "Food"
+    },
+    {
+        brand: "Maza by Popular Groubs of Industries",
+        productName: "Juices & Squashes",
+        category: "Food"
+    },
+    {
+        brand: "Mitchell's",
+        productName: "Ketchup,Mayo & Jams",
+        category: "Food"
+    },
+    {
+        brand: "National",
+        productName: "Ketchup,Mayo & Jams",
+        category: "Food"
+    },
+    {
+        brand: "Young's",
+        productName: "Ketchup,Mayo & Jams",
+        category: "Food"
+    },
+    {
+        brand: "Shezan",
+        productName: "Ketchup,Mayo & Jams",
+        category: "Food"
+    },
+    {
+        brand: "Bisconni",
+        productName: "Biscuits",
+        category: "Food"
+    },
+    {
+        brand: "Mayfair",
+        productName: "Biscuits",
+        category: "Food"
+    },
+    {
+        brand: "Whistlez",
+        productName: "Biscuits",
+        category: "Food"
+    },
+    {
+        brand: "Shan Shoop Noodles",
+        productName: "Noodles",
+        category: "Food"
+    },
+    {
+        brand: "Brighto Paints",
+        productName: "Paints",
+        category: "grocery"
+    },
+    {
+        brand: "Master Paints",
+        productName: "Paints",
+        category: "grocery"
+    },
+    {
+        brand: "Happilac Paints",
+        productName: "Paints",
+        category: "grocery"
+    },
+    {
+        brand: "Al Asal",
+        productName: "Honey",
+        category: "Food"
+    },
+    {
+        brand: "Hashmi",
+        productName: "Honey",
+        category: "Food"
+    },
+    {
+        brand: "Young's",
+        productName: "Honey",
+        category: "Food"
+    },
+    {
+        brand: "Bio Amla",
+        productName: "Shampoos",
+        category: "grocery"
+    },
+    {
+        brand: "Saeed Ghani",
+        productName: "Shampoos",
+        category: "grocery"
+    },
+    {
+        brand: "Necos",
+        productName: "Shampoos",
+        category: "grocery"
+    },
+    {
+        brand: "Sufi Super",
+        productName: "Surf",
+        category: "grocery"
+    },
+    {
+        brand: "Gai Power Wash",
+        productName: "Surf",
+        category: "grocery"
+    },
+    {
+        brand: "Rooh Afza",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "Jam-e-shirin",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "Sun Dip",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "Limopani",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "National",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "Shezan",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "Quice",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "Hamdard",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "Servis",
+        productName: "Shoes",
+        category: "Fasion"
+    },
+    {
+        brand: "Stylo",
+        productName: "Shoes",
+        category: "Fasion"
+    },
+    {
+        brand: "Panther",
+        productName: "Tyres",
+        category: "grocery"
+    },
+    {
+        brand: "General Tyres",
+        productName: "Tyres",
+        category: "grocery"
+    },
+    {
+        brand: "PSO",
+        productName: "Oil & Fuel",
+        category: "grocery"
+    },
+    {
+        brand: "ATTOCK",
+        productName: "Oil & Fuel",
+        category: "grocery"
+    },
+    {
+        brand: "GO",
+        productName: "Oil & Fuel",
+        category: "grocery"
+    },
+    {
+        brand: "ASKARI 1",
+        productName: "Oil & Fuel",
+        category: "grocery"
+    },
+    {
+        brand: "BAKRI",
+        productName: "Oil & Fuel",
+        category: "grocery"
+    },
+    {
+        brand: "Simply Sufi",
+        productName: "Food Chains",
+        category: "Food"
+    },
+    {
+        brand: "English Biscuits",
+        productName: "Biscuits",
+        category: "Food"
+    },
+    {
+        brand: "English",
+        productName: "Ketchup,Mayo & Jams",
+        category: "Food"
+    },
+    {
+        brand: "Nelson Paints",
+        productName: "Paints",
+        category: "grocery"
+    },
+    {
+        brand: "English Shampoos",
+        productName: "Shampoos",
+        category: "grocery"
+    },
+    {
+        brand: "Hemani",
+        productName: "Shampoos",
+        category: "grocery"
+    },
+    {
+        brand: "Mitchell's",
+        productName: "Drinks & Syrups",
+        category: "Food"
+    },
+    {
+        brand: "HASCOL",
+        productName: "Oil & Fuel",
+        category: "grocery"
+    },
+    {
+        brand: "Howdy",
+        productName: "Food Chains",
+        category: "Food"
+    }
+]
+
+
+export default productData;
